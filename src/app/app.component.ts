6f9f6e3b44eb9f6fea5882e00fd4a80a401b1ab7
arpit05va <arpitvashistha928@gmail.com>
2022-10-15 10:04:21 +0530

    intital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dataparentchild';
+  title = 'Send Data Child to parent Component';
+  name: string = "Arpit";
+  updateChild() {
+    this.name = "Shreyansh";
+  }
 }
